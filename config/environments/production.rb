29d04c3e541f0ca12d41603199d53b5b166de618
kazu1212-star <12matumatu04@gmail.com>
2023-05-18 21:51:34 +0900

    mailgun設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "myapp_production"
-  config.action_mailer.default_url_options = { host: 'https://kazu-twitter-clone.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
