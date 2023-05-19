26c7202222b40001bdc5f0188f04e32dd0aa7728
kazu1212-star <12matumatu04@gmail.com>
2023-05-20 07:10:29 +0900

    ログアウトできるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   resources :tweets, only: :index
   root to: 'tweets#index'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
