c5d06cff76a31bff11ea4fd9ea508069ccf1fece
kazu1212-star <12matumatu04@gmail.com>
2023-06-15 21:20:27 +0900

    seed 変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 jone = User.create!({
                       name: 'Jone',
-                      email: 'jone@example.com',
+                      email: 'jone@test.com',
                       password: '********',
                       telephone_number: 111_111,
                       date_of_birth: Date.new(2000, 1, 1)
@@ -21,7 +21,7 @@ jone.save!
 
 nick = User.create!({
                       name: 'Nick',
-                      email: 'nick@example.com',
+                      email: 'nick@test.com',
                       password: '********',
                       telephone_number: 111_111,
                       date_of_birth: Date.new(2000, 1, 1)
@@ -33,7 +33,7 @@ nick.save!
 
 maria = User.create!({
                        name: 'Maria',
-                       email: 'maria@example.com',
+                       email: 'maria@test.com',
                        password: '********',
                        telephone_number: 111_111,
                        date_of_birth: Date.new(2000, 1, 1)
@@ -43,17 +43,6 @@ maria.icon.attach(io: File.open(Rails.root.join('app/assets/images/banana.jpg'))
 maria.skip_confirmation!
 maria.save!
 
-emma = User.create!({
-                      name: 'Emma',
-                      email: 'emma@example.com',
-                      password: '********',
-                      telephone_number: 111_111,
-                      date_of_birth: Date.new(2000, 1, 1)
-                    })
-emma.icon.attach(io: File.open(Rails.root.join('app/assets/images/book.jpg')),
-                 filename: 'book.jpeg')
-emma.skip_confirmation!
-emma.save!
 
 jone.tweets.create!(text: 'Test tweet from John')
 jone.tweets.create!(text: 'Hello from John')
@@ -61,10 +50,8 @@ nick.tweets.create!(text: 'Test tweet from Nick')
 nick.tweets.create!(text: 'Hello')
 maria.tweets.create!(text: 'Test tweet from Maria')
 maria.tweets.create!(text: 'Hello from Maria')
-emma.tweets.create!(text: 'Test tweet from Emma')
-emma.tweets.create!(text: 'Hello from Emma')
+
 
 jone.following << nick
 nick.following << maria
-maria.following << emma
-emma.following << jone
+maria.following << jone
