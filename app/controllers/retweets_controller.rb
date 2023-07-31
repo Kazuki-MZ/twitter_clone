5cc646d26adec9b2c1f27c42a52e57f1831eda18
kazu1212-star <12matumatu04@gmail.com>
2023-07-31 14:15:10 +0900

    リツイート機能実装

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -4,4 +4,15 @@ class RetweetsController < ApplicationController
   def index
     @retweet_tweets = current_user.retweet_tweets.order('retweets.created_at DESC').page(params[:page]).per(5)
   end
+
+  def create
+    current_user.retweets.create!(tweet_id: params[:tweet_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    retweet = current_user.retweets.find_by!(tweet_id: params[:tweet_id])
+    retweet.destroy!
+    redirect_back(fallback_location: root_path)
+  end
 end
