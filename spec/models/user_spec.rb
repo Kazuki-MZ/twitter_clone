2732084932b5156e3a91ef51527660278e986b35
kazu1212-star <12matumatu04@gmail.com>
2023-08-31 12:15:45 +0900

    ユーザーmodelのvalidation　単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:user) { build(:user) }
+
+    it '全ての項目の入力が存在すれば登録できること' do
+      expect(user).to be_valid
+    end
+
+    it 'nameがない場合は登録できないこと' do
+      user.name = nil
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+
+    it 'emailがない場合は登録できないこと' do
+      user.email = nil
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+
+    it 'passwordがない場合は登録できないこと' do
+      user.password = nil
+      user.valid?
+      expect(user.errors[:password]).to include('を入力してください')
+    end
+
+    it 'telephone_numberがない場合は登録できないこと' do
+      user.telephone_number = nil
+      user.valid?
+      expect(user.errors[:telephone_number]).to include('を入力してください')
+    end
+
+    it 'date_of_birthがない場合は登録できないこと' do
+      user.date_of_birth = nil
+      user.valid?
+      expect(user.errors[:date_of_birth]).to include('を入力してください')
+    end
+
+    it '適切なemailだけを許可する' do
+      user.email = '@1234ww@567'
+      user.valid?
+      expect(user.errors[:email]).to include('は不正な値です')
+    end
+  end
 end
